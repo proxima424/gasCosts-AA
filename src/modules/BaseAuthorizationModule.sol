bbb9f47b9f959d1257a03c965d47eaada5cedbfe
proxima424 <proxima42ly@gmail.com>
2023-07-03 19:50:22 +0530

    initialized AA repo in foundry

---
+++ src/modules/BaseAuthorizationModule.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {IAuthorizationModule, UserOperation} from "../interfaces/IAuthorizationModule.sol";
+import {ISignatureValidator, ISignatureValidatorConstants} from "../interfaces/ISignatureValidator.sol";
+
+abstract contract BaseAuthorizationModule is
+    IAuthorizationModule,
+    ISignatureValidator
+{
+    uint256 internal constant SIG_VALIDATION_FAILED = 1;
+}
